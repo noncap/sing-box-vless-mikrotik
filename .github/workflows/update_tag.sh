47086d611745aa4991260e1699983c24c0f0e3d9
Anton Kudryavtsev <boblobl4@gmail.com>
2025-03-11 19:31:01 +0300

    fail version check if even one version is missing

--- .github/workflows/update_tag.sh
+++ .github/workflows/update_tag.sh
@@ -4,7 +4,7 @@ git config --local user.email "41898282+github-actions[bot]@users.noreply.github
 
 TagRemote=$(curl -s https://api.github.com/repos/SagerNet/sing-box/releases | jq -r '.[] | select(.prerelease == false) | .tag_name' | head -n1)
 TagDevRemote=$(curl -s https://api.github.com/repos/SagerNet/sing-box/releases | jq -r '.[] | select(.prerelease == true) | .tag_name' | head -n1)
-if [ -z "${TagRemote}" ] && [ -z "${TagDevRemote}" ]; then
+if [ -z "${TagRemote}" ] || [ -z "${TagDevRemote}" ]; then
   echo "Version check failed!"
   exit 1
 fi
